3255033405dce4e45760da04cf470dcea3cba578
Jérémy DECOOL <contact@jdecool.fr>
2025-11-12 22:55:11 +0100

    Upgrade PHP CS Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,8 +11,8 @@ $config
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PER-CS2.0' => true,
-        '@PER-CS2.0:risky' => true,
+        '@PER-CS2x0' => true,
+        '@PER-CS2x0:risky' => true,
         'declare_strict_types' => true,
         'no_unused_imports' => true,
     ])
